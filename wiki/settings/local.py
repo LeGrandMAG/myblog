db6539cdac678340ae52ad0cc7edef7e821d2540
LeGrandMAG <magmukendi@hufs.ac.kr>
2022-06-23 03:24:40 +0900

    added static files

--- wiki/settings/local.py
+++ wiki/settings/local.py
@@ -18,4 +18,4 @@ ALLOWED_HOSTS = ['*']
 """
 ##STATIC_ROOT = os.path.join(BASE_DIR, 'live-static-files', 'static-root')
 
-"""MEDIA_ROOT = os.path.join(BASE_DIR, 'live-static', 'media-root')"""
+MEDIA_ROOT = os.path.join(BASE_DIR, 'live-static', 'media-root')
