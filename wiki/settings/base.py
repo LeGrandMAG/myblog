f07dfb571fe6c182933bf8f30432b95c5046a65e
LeGrandMAG <magmukendi@hufs.ac.kr>
2022-06-24 09:33:41 +0900

    ABC

--- wiki/settings/base.py
+++ wiki/settings/base.py
@@ -127,12 +127,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
-STATIC_URL = '/static/'
-STATICFILES_DIRS = [
-    (os.path.join(BASE_DIR, 'static')),
-]
+STATIC_URL = 'static/'
+STATICFILES_DIRS =  [ BASE_DIR / "static"]
 
-STATIC_ROOT = os.path.join(BASE_DIR, "live-static")
+
+STATIC_ROOT = "live-static/static"
 
 MEDIA_URL = '/media/'
 
